5ae3bd8365078f27691e08637adb57ad1981eaf8
Guidy ange isaac <packer02andro69@gmail.com>
2024-03-17 15:45:55 +0100

    projet_tailwindcss

---
+++ projet_tailwindcss/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {fontFamily: {
+    sans: ['montserrat', 'sans-serif'],
+    serif: ['manrope', 'sans-serif'],
+    colors: {
+      btnA : "f2d534",
+    }
+  },
+    extend: {},
+  },
+  plugins: [],
+}
