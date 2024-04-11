f5ffe0e2967f060bf56b1cf94a609bb39a0cfa1b
Guidy ange isaac <packer02andro69@gmail.com>
2024-04-11 08:10:43 +0100

    ajout de la fonction qui determine l'heure

--- requette_reseau/src/index.js
+++ requette_reseau/src/index.js
@@ -26,7 +26,7 @@ function date (){
 function temperature(data){
     temp.textContent           = Math.round(data.main.temp) + " °C";
     icon.innerHTML             = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="" class="taile"/>` ;
-    nomVille.innerText         = `ville: ${data.name}`;
+    nomVille.innerText         = `${data.name}`;
     description.innerHTML      = data.weather[0].description;
     vitesseVent.innerText      = data.wind.speed + " m/s"
     humidite.innerText         = data.main.humidity + "%"
